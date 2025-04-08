b9b0fec7322b44f2a242d97904b89807a6b15294
hxbx.47 <hobyhardiot1@gmail.com>
2025-04-08 12:02:46 +0300

    update chef service

--- frontend/src/views/dashboard/chef-service/ChefServiceEdit.jsx
+++ frontend/src/views/dashboard/chef-service/ChefServiceEdit.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-function ChefServiceEdit() {
+import Modal from '../../../components/ui/Modal';
+
+function ChefServiceEdit({ isOpen, chauffeur, onChange, onSave, onClose }) { 
+ 
+  // Fallback si `chauffeur` est null/undefined
+  const validChauffeur = chauffeur || {};
+ 
+  // État local pour valider le formulaire
+  const [isFormValid, setIsFormValid] = useState(true);
+  // Vérifie si tous les champs requis sont remplis correctement
+  const checkFormValidity = () => {
+    const { nom = '', prenom = '', permis_conduire = [], experience = 1 } = validChauffeur;
+    const isValid = nom.trim() !== '' && prenom.trim() !== '' && permis_conduire.length > 0 && experience > 0;
+    setIsFormValid(isValid);
+  };
+  
+   // Fonction pour réinitialiser le formulaire
+   const resetForm = () => {
+     onChange({
+       nom: '',
+       prenom: '', 
+     });
+   };
+  
+  
+  // Chaque fois que le `chauffeur` change, on vérifie la validité du formulaire
+  useEffect(() => {
+    checkFormValidity();
+  }, [validChauffeur]);
+  
   return (
-    <div>ChefServiceEdit</div>
-  )
+    <Modal
+      title="Modifier chef service"
+      btnLabel="Sauvegarder"
+      isOpen={isOpen}
+      onSave={() => onSave(validChauffeur)}
+      onClose={onClose}
+      isFormValid={isFormValid}
+      resetForm={resetForm}
+    >
+ 
+    </Modal>
+  );
 }
 
-export default ChefServiceEdit
+export default ChefServiceEdit;
