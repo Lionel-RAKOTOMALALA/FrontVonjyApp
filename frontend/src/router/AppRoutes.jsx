e146e0a40767aa630ae3f285d357e8b87b7ffb8a
hxbx.47 <hobyhardiot1@gmail.com>
2025-04-09 18:05:05 +0300

    Problem fixed

--- frontend/src/router/AppRoutes.jsx
+++ frontend/src/router/AppRoutes.jsx
@@ -37,6 +37,7 @@ const AppRoutes = ({user, isLoggedIn}) => {
             <Route path="/fokontany" element={<Layout user={user}><Fokontany user={user}/></Layout>}/>
             <Route path="/service" element={<Layout user={user}><Service user={user}/></Layout>}/>
             <Route path="/chef-service" element={<Layout user={user}><ChefService user={user}/></Layout>}/>
+            <Route path="/composant" element={<Layout user={user}><Composant user={user}/></Layout>}/>
 
             <Route path="/map" element={<Map />}/>
 
