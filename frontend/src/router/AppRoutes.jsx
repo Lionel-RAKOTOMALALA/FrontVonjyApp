bdaf441c227f6939dc9b49b4f81a6696101a3042
hxbx.47 <hobyhardiot1@gmail.com>
2025-04-19 15:30:13 +0300

    Remove unused layout and UI component files

--- frontend/src/router/AppRoutes.jsx
+++ frontend/src/router/AppRoutes.jsx
@@ -6,8 +6,7 @@ import Security from '../views/account/security';
 import Logout from '../views/logout';
 import NotFound from '../views/misc/notfound';
 import Layout from "../layouts/Layout";
-import PrivateRoute from "../layouts/PrivateRoute"; 
-import Chauffeur from '../views/dashboard/chauffeur/ChauffeurViews';
+import PrivateRoute from "../layouts/PrivateRoute";  
 import Commune from '../views/dashboard/commune/CommuneViews';
 import Fokontany from '../views/dashboard/fokontany/FokontanyViews';
 import Responsable from '../views/dashboard/responsable/ResponsableViews';
@@ -29,8 +28,7 @@ const AppRoutes = ({user, isLoggedIn}) => {
                 </PrivateRoute>}/>
             <Route path="/account/profile" element={<Profile user={user}/>}/>
             <Route path="/account/security" element={<Security user={user}/>}/>
- 
-            <Route path="/chauffeur" element={<Layout user={user}><Chauffeur user={user}/></Layout>}/>
+  
             <Route path="/commune" element={<Layout user={user}><Commune user={user}/></Layout>}/>
             <Route path="/fokontany" element={<Layout user={user}><Fokontany user={user}/></Layout>}/>
             <Route path="/responsable" element={<Layout user={user}><Responsable user={user}/></Layout>}/>
